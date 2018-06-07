b64a77073d20f99b87174bddf6dcc84d954e55f4
Vitaly Popuzin <popuzin@gmail.com>
2018-06-08 00:59:07 +0300

    Добавил базовый класс Контроля Игрока и класс Перемещения Персонажа (подходит в том числе и для NPC). Создал на сцене Player'a в виде круглешка

---
+++ GB-project/Assets/Scripts/PlayerController/CharacterMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    protected float moveSpeed = 1f;
+	
+    public void Move(Vector3 movement)
+    {
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
---
+++ GB-project/Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿/* 
+ Пока выполняет только роль PlayerInputController 
+ т.е. получает ввод и двигает за счет этого персонажа
+ */
+using UnityEngine;
+
+namespace GBproject.SceneObjects
+{
+    [RequireComponent(typeof(CharacterMovement))]
+    public class PlayerController : BaseSceneObject
+    {
+        private CharacterMovement characterMovement;
+        private Vector3 movement;
+        void Awake()
+        {
+            characterMovement = GetComponent<CharacterMovement>();
+        }
+
+        void Update()
+        {
+            movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            characterMovement.Move(movement);
+        }
+    }
+}
+
