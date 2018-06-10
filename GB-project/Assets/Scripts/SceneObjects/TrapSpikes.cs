70491d9f94e6ffe6c1c9bf8d4db9a14166872c2e
Unknown <martyushevsky@gmail.com>
2018-06-10 13:53:39 +0300

    Добавил класс ловушек типа "шипы" TrapSpikes

---
+++ GB-project/Assets/Scripts/SceneObjects/TrapSpikes.cs
@@ -0,0 +1,32 @@
+﻿using GBproject.Interfaces;
+using UnityEngine;
+
+namespace GBproject.SceneObjects
+{
+    [DisallowMultipleComponent]
+    public class TrapSpikes : BaseTrap, IDamageDealer
+    {
+        [SerializeField]
+        [Tooltip("Наносимый урон")]
+        private float _damage;
+
+        public float GetDamage()
+        {
+            return _damage;
+        }
+
+        protected override void Interact(Collision2D collision)
+        {
+            var _damagableObject = collision.collider.gameObject.GetComponent<IDamagable>();
+            if (_damagableObject != null)
+            {
+                DealDamage(_damagableObject);
+            }
+        }
+
+        private void DealDamage(IDamagable _damagableObject)
+        {
+            _damagableObject.ApplyDamage(this as IDamageDealer);
+        }
+    }
+}
