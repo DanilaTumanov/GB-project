b6d5731f3db3cd407d3314813b102b76beaf659e
Vitaly Popuzin <popuzin@gmail.com>
2018-06-13 10:22:59 +0300

    Приступил к работе над вертикальным перемещением

---
+++ GB-project/Assets/Scripts/SceneObjects/Characters/BaseCharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace GBproject
+{
+    [RequireComponent(typeof(Rigidbody2D))]    
+    /// <summary>
+    /// Базовый класс передвижения персонажей
+    /// </summary>
+    public abstract class BaseCharacterMovement : MonoBehaviour
+    {
+        protected Rigidbody2D _rb2D;
+        protected Vector2 _newVelocity;
+
+        [Tooltip("Скорость перемещения персонажа. Умножает введенный вектор перемещения")]
+        [SerializeField]
+        protected float _maxSpeed = 1f;
+        
+        protected virtual void Awake()
+        {
+            _rb2D = GetComponent<Rigidbody2D>();
+        }
+
+        /// <summary>
+        /// Перемещение персонажа в указанном направлении.
+        /// </summary>
+        /// <param name="movement">направление и величина перемещения</param>
+        public abstract void Move(Vector2 moveVector);
+    }
+}
---
+++ GB-project/Assets/Scripts/SceneObjects/Characters/PositionCharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace GBproject
+{    
+    public class PositionCharacterMovement : BaseCharacterMovement
+    { 
+        private Vector2 _nextMove = Vector2.zero;
+        private Vector2 _prevPos;
+        private Vector2 _targetPos;
+
+        [SerializeField]
+        private float _acceleration;
+
+        [SerializeField]
+        private float _decceleration;
+       
+        public override void Move(Vector2 moveVector)
+        {
+            float desiredSpeed = moveVector.x * _maxSpeed;
+            float acceleration = Input.GetAxisRaw("Horizontal") != 0 ? _acceleration : _decceleration;
+
+            moveVector.x = Mathf.MoveTowards(moveVector.x, desiredSpeed * Time.deltaTime, _acceleration * Time.deltaTime);
+
+            _nextMove += moveVector;
+
+            _prevPos = _rb2D.position;
+            _targetPos = _prevPos + _nextMove;
+            _newVelocity = (_targetPos - _prevPos) / Time.deltaTime;
+
+            _rb2D.MovePosition(_targetPos);
+
+            _nextMove = Vector2.zero;
+        }      
+    }
+}
